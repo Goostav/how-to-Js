1ed6defaa7a6605cc533de15f9f34239aed6c18f
Michał Olszewski <mic.olszew@gmail.com>
2018-05-01 16:12:14 +0200

    change content in index.html & script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,5 @@
 
-
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-/*
-above is first alert-be aware
-*/
-console.log('Hello, ' + name);
-
-//This is second comment
-//and last i hope
+var a = 4,
+    h = 6,
+    triangleArea = a * h / 2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
