8754d731fa437b0acc9585c28eb3d0869fe6cdeb
Michał Olszewski <mic.olszew@gmail.com>
2018-05-02 12:53:54 +0200

    change index.html, script.js & package.json

--- js/script.js
+++ js/script.js
@@ -1,16 +1,9 @@
 
-var a = 137;
-var b = 9;
+var a = 0;
+var b = 0;
 var value = (a * a) - (2 * a * b) - (b * b);
 
 console.log(value);
 
-if (value > 0) {
-    document.write('wynik dodatni');
-}
-else if (value < 0) {
-    document.write('wynik ujemny');
-}
-else {
-    document.write('wynik jest równy 0');
-}
+(value > 0) ? document.write('wynik dodatni') : document.write('wynik ujemny');
+
