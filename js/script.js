d6cc1ce450bcdf365a1e6497e3d22098a923943b
Michał Olszewski <mic.olszew@gmail.com>
2018-05-06 19:04:16 +0200

    change script.js again

--- js/script.js
+++ js/script.js
@@ -6,10 +6,22 @@ var triangle3Area = getTriangleArea(5, 13);
 function getTriangleArea(a, h) {    
     
     if  ((a <= 0) || (h <= 0)) {
-        document.write('Nieprawidłowe dane');
+        return 'Nieprawidłowe dane';
     } else {
         return (a*h/2);
     } 
-}
+ }
 
 console.log(getTriangleArea(10, 6))
+
+
+
+
+
+
+
+
+
+
+
+
