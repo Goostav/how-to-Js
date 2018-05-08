f07204c5c9ecff571f4096ad5bc4def8e8ea68d1
Michał Olszewski <mic.olszew@gmail.com>
2018-05-08 19:10:41 +0200

    change index.html & script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
-function drawTree(l) {
+window.onload = function() {
+  console.log('The page is fully loaded.');
+  
+    var withButtonItemsClass = document.getElementsByClassName('button');
     
-    for (var i = 0; i < l; i++) {
-        var star = '';
-        for (var j = 0; j < i; j++) {
-            star += '*';
-        }
-        console.log(star);  
-    } 
-    return star+= '*';
+        for (var i = 0; i < withButtonItemsClass.length; i++) {
+            alert(withButtonItemsClass[i].innerText);
+        }  
 }
 
-console.log(drawTree(9));
-
+/*
+function drawTree(l) {
+     
++    for (var i = 0; i < l; i++) {
++        var star = '';
++        for (var j = 0; j < i; j++) {
++            star += '*';
++        }
++        console.log(star);  
++    } 
++    return star+= '*';
+ }
++console.log(drawTree(9));
+*/
